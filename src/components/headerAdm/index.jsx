c4544a24d3f4b8a099859686865974565ba5a8f7
Murilo <murigod8@gmail.com>
2025-09-17 19:13:13 -0300

    Added adm header

---
+++ src/components/headerAdm/index.jsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import "./header.css";
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  function Sair() {
+    localStorage.clear();
+    navigate("/login");
+  }
+
+  return (
+    <header className="header-container">
+      <div className="logo" onClick={() => navigate("/")}>
+        <span className="logo-cine">Cine</span>
+        <span className="logo-mão">mão</span>
+      </div>
+
+      <nav className="nav-buttons">
+        <button onClick={() => navigate("/")} className="nav-btn">
+          Home
+        </button>
+        <button onClick={() => navigate("/profile")} className="nav-btn">
+          Perfil
+        </button>
+        <button onClick={() => navigate("/admin")} className="nav-btn">
+          Admin
+        </button>
+        <button onClick={Sair} className="nav-btn">
+          Sair
+        </button>
+      </nav>
+    </header>
+  );
+}
