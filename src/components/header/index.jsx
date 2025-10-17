c986d41eff53b2e2c2c61e411b48fb22661b34ff
Murilo <152417228+murilofernandes1@users.noreply.github.com>
2025-10-17 11:51:16 -0300

    fixed: Header fixed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,41 +1,35 @@
 import { useNavigate } from "react-router-dom";
-
 import "./header.css";
 
 export default function Header() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
+
   function Sair() {
     localStorage.clear();
     navigate("/login");
   }
-  
+
   return (
-    {token ?  (
     <header className="header-container">
       <div className="logo" onClick={() => navigate("/")}>
         <span className="logo-cine">Cine</span>
         <span className="logo-mão">mão</span>
       </div>
-      <nav className="nav-buttons">
-        <button onClick={() => navigate("/")} className="nav-btn">
-          Home
-        </button>
-        <button onClick={() => navigate("/profile")} className="nav-btn">
-          Minha conta
-        </button>
-        <button onClick={Sair} className="nav-btn">
-          Sair
-        </button>
-      </nav>
+
+      {token ? (
+        <nav className="nav-buttons">
+          <button onClick={() => navigate("/")} className="nav-btn">
+            Home
+          </button>
+          <button onClick={() => navigate("/profile")} className="nav-btn">
+            Minha conta
+          </button>
+          <button onClick={Sair} className="nav-btn">
+            Sair
+          </button>
+        </nav>
+      ) : null}
     </header>
-) : (
-    <header className="header-container">
-   <div className="logo" onClick={() => navigate("/")}>
-        <span className="logo-cine">Cine</span>
-        <span className="logo-mão">mão</span>
-      </div>
-      </header/>
-)}
-  )
+  );
 }
